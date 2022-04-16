a77a6a0e78be52fbd52892a2b154624ff0a65dec
Erykzon <jota.once6mail.com>
2022-04-16 18:37:53 -0500

    add example1-13

---
+++ weekly_mission_2/examples1/example1-13.js
@@ -0,0 +1,4 @@
+// Ejemplo 13: Uso de findIndex, este método regresa la posición del primer elemento que cumpla con la validación que indiques.
+const names13 = ['Exp 1', 'Explorer 2', 'Explorer 3','Aqui esta el elemento']
+const result = names13.findIndex((name) => name.length > 7)
+console.log("Ejemplo 13: Primer elemento cuya palabra sea mayor a 7 esta en la posición "+ result)
